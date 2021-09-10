f794b5252e3ab15321bdb9f2ccd2d46ebc693f78
nephilister <olegvastaev@gmail.com>
2021-09-10 19:17:50 +0300

    list

--- src/make_list.c
+++ src/make_list.c
@@ -6,7 +6,7 @@
 /*   By: nephilister <nephilister@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 04:24:51 by nephilister       #+#    #+#             */
-/*   Updated: 2021/09/10 18:20:22 by nephilister      ###   ########.fr       */
+/*   Updated: 2021/09/10 19:02:43 by nephilister      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,16 @@ t_redir	*new_redirect(int t)
 	new->next = NULL;
 }
 
+void	add_redirect(t_redir **lst, t_redir *new)
+{
+	t_redir *ptr;
+	
+	if (lst == NULL)
+		*lst = new;
+	else
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;		
+		ptr->next = new;
+	}
+}
